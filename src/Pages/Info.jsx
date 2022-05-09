22550545da3525f7c9d930181e81d58e288316c1
Abhas K <67013564+iamabhas@users.noreply.github.com>
2022-05-09 14:10:23 +0545

    updated info.jsx

--- src/Pages/Info.jsx
+++ src/Pages/Info.jsx
@@ -6,15 +6,22 @@ function Info() {
   return (
     <div className="error-container">
       <h1>Made by Abhas Khanal</h1>
+      <br />
       <h2>
-        # If you want to get all pokemons after searching , click on the
+        - This website does not look good on mobile but its still responsive
+      </h2>
+      <br />
+      <h2>
+        - If you want to get all pokemons after searching , click on the
         "POKEDEX" logo at the top left
       </h2>
+      <br />
       <h2>
-        # The data for the pokemons are fetched from the PokeAPI . Click on the
+        - The data for the pokemons are fetched from the PokeAPI . Click on the
         pokeball on topright of page for the api
       </h2>
-      <h2># 151 pokemons are fetched</h2>
+      <br />
+      <h2>- 151 pokemons are fetched</h2>
       <button
         className="btn"
         onClick={() => {
